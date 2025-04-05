81d100b8392093f819680c23b17d3ccebe67655b
Ahmad Shahzad <107808273+shzdehmd@users.noreply.github.com>
2025-04-05 09:47:00 +0500

    Passing the url to the 404 page.

--- utils/middlewares.js
+++ utils/middlewares.js
@@ -1,4 +1,4 @@
 // Middleware to handle 404 routes.
-const error404 = (req, res) => res.status(404).render('404.html');
+const error404 = (req, res) => res.status(404).render('404.html', { url: process.env.URL });
 
 module.exports = { error404 };
